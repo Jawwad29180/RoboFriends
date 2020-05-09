a85f6046b443a7b400e62d7b59204e28c8c07d22
ABATGROUP\JMuhammed <jawwad.muhammed@abatplus.de>
2020-05-09 12:27:48 +0200

    Updates

---
+++ precache-manifest.49eca01d87618ab9c52f4f44e368a95c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8329a2ce6ce6584d1be434656d2ab1b5",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/RoboFriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "fe2279841bbf791715ed",
+    "url": "/RoboFriends/static/css/main.6661cdab.chunk.css"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/RoboFriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2279841bbf791715ed",
+    "url": "/RoboFriends/static/js/main.9a64f925.chunk.js"
+  },
+  {
+    "revision": "9fda7ca17e43078694ea",
+    "url": "/RoboFriends/static/js/runtime-main.7cecb56b.js"
+  }
+]);
